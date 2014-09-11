6a112432c6c6ce62cdb3d4d69ba9ba4cc4afcaf1
Frank Wilson <frank@zenlambda.com>
2014-09-11 18:42:59 +0100

    Remove unnecessary packaging field

--- fabric8-maven-plugin/src/test/java/io/fabric8/maven/stubs/CreateProfileArtifactStub.java
+++ fabric8-maven-plugin/src/test/java/io/fabric8/maven/stubs/CreateProfileArtifactStub.java
@@ -13,100 +13,65 @@ public class CreateProfileArtifactStub
 
     private String version;
 
-    private String packaging;
-
     private VersionRange versionRange;
 
     private ArtifactHandler handler;
 
-    /**
-     * @param groupId
-     * @param artifactId
-     * @param version
-     * @param packaging
-     */
     public CreateProfileArtifactStub( String groupId, String artifactId,
-                                          String version, String packaging )
+                                          String version, String type )
     {
         this.groupId = groupId;
         this.artifactId = artifactId;
         this.version = version;
-        this.packaging = packaging;
-        setType(packaging);
+        setType(type);
         versionRange = VersionRange.createFromVersion( version );
     }
 
-    /** {@inheritDoc} */
     public void setGroupId( String groupId )
     {
         this.groupId = groupId;
     }
 
-    /** {@inheritDoc} */
     public String getGroupId()
     {
         return groupId;
     }
 
-    /** {@inheritDoc} */
     public void setArtifactId( String artifactId )
     {
         this.artifactId = artifactId;
     }
 
-    /** {@inheritDoc} */
     public String getArtifactId()
     {
         return artifactId;
     }
 
-    /** {@inheritDoc} */
     public void setVersion( String version )
     {
         this.version = version;
     }
 
-    /** {@inheritDoc} */
     public String getVersion()
     {
         return version;
     }
 
-    /**
-     * @param packaging
-     */
-    public void setPackaging( String packaging )
-    {
-        this.packaging = packaging;
-    }
-
-    /**
-     * @return the packaging
-     */
-    public String getPackaging()
-    {
-        return packaging;
-    }
-
-    /** {@inheritDoc} */
     public VersionRange getVersionRange()
     {
         return versionRange;
     }
 
-    /** {@inheritDoc} */
     public void setVersionRange( VersionRange versionRange )
     {
         this.versionRange = versionRange;
     }
 
-    /** {@inheritDoc} */
     public ArtifactHandler getArtifactHandler()
     {
         return handler;
     }
 
-    /** {@inheritDoc} */
     public void setArtifactHandler( ArtifactHandler handler )
     {
         this.handler = handler;
