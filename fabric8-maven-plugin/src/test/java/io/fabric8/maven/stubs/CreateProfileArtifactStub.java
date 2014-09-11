9ea1645e878056fb9648768853878f0886b82767
Frank Wilson <frank@zenlambda.com>
2014-09-11 18:54:29 +0100

    Remove unecessary overrides from ArtifactStub

--- fabric8-maven-plugin/src/test/java/io/fabric8/maven/stubs/CreateProfileArtifactStub.java
+++ fabric8-maven-plugin/src/test/java/io/fabric8/maven/stubs/CreateProfileArtifactStub.java
@@ -7,73 +7,38 @@ import org.apache.maven.plugin.testing.stubs.ArtifactStub;
 public class CreateProfileArtifactStub
         extends ArtifactStub
 {
-    private String groupId;
 
-    private String artifactId;
-
-    private String version;
-
+    /**
+     * ArtifactStub does not store version range, so we store it here.
+     */
     private VersionRange versionRange;
-
-    private ArtifactHandler handler;
 
     public CreateProfileArtifactStub( String groupId, String artifactId,
                                           String version, String type )
     {
-        this.groupId = groupId;
-        this.artifactId = artifactId;
-        this.version = version;
+        setGroupId(groupId);
+        setArtifactId(artifactId);
+        setVersion(version);
         setType(type);
         versionRange = VersionRange.createFromVersion( version );
     }
 
-    public void setGroupId( String groupId )
-    {
-        this.groupId = groupId;
-    }
-
-    public String getGroupId()
-    {
-        return groupId;
-    }
-
-    public void setArtifactId( String artifactId )
-    {
-        this.artifactId = artifactId;
-    }
-
-    public String getArtifactId()
-    {
-        return artifactId;
-    }
-
-    public void setVersion( String version )
-    {
-        this.version = version;
-    }
-
-    public String getVersion()
-    {
-        return version;
-    }
-
+    /**
+     * Gets the stored version range
+     * @return version range
+     */
     public VersionRange getVersionRange()
     {
         return versionRange;
     }
 
+    /**
+     * Sets the version range instead of discarding it (as in the parent class)
+     * @param versionRange the version range
+     */
     public void setVersionRange( VersionRange versionRange )
     {
         this.versionRange = versionRange;
     }
 
-    public ArtifactHandler getArtifactHandler()
-    {
-        return handler;
-    }
-
-    public void setArtifactHandler( ArtifactHandler handler )
-    {
-        this.handler = handler;
-    }
 }
